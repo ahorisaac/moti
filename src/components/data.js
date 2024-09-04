1beaeda78b982846631dcc50cb2b839c58c96c03
ahorisaac <ahorisaac.ai@gmail.com>
2024-09-04 21:14:16 +0100

    chore: create the data file

---
+++ src/components/data.js
@@ -0,0 +1,58 @@
+const data = [
+  {
+    id: 0,
+    name: "Alloy Gear Shifter",
+    price: "$9.99",
+    image: require("../assets/images/image1.jpg"),
+  },
+  {
+    id: 1,
+    name: "Bamboo Keyboard",
+    price: "$14.99",
+    image: require("../assets/images/image2.jpg"),
+  },
+  {
+    id: 2,
+    name: "Solar Powerbank",
+    price: "$19.99",
+    image: require("../assets/images/image3.jpg"),
+  },
+  {
+    id: 3,
+    name: "Bluetooth Headphones",
+    price: "$7.99",
+    image: require("../assets/images/image4.jpg"),
+  },
+  {
+    id: 4,
+    name: "Carbon Fiber Tripod",
+    price: "$12.49",
+    image: require("../assets/images/image5.jpg"),
+  },
+  {
+    id: 5,
+    name: "Motion-Activated Lighting",
+    price: "$16.99",
+    image: require("../assets/images/image6.jpg"),
+  },
+  {
+    id: 6,
+    name: "Magnetic Desk Organizer",
+    price: "$11.25",
+    image: require("../assets/images/image7.jpg"),
+  },
+  {
+    id: 7,
+    name: "Outdoor Action Camera",
+    price: "$8.75",
+    image: require("../assets/images/image8.jpg"),
+  },
+  {
+    id: 8,
+    name: "Charging Pad Indicator",
+    price: "$13.99",
+    image: require("../assets/images/image9.jpg"),
+  },
+];
+
+export default data;
